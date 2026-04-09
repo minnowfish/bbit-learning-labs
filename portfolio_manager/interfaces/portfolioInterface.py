1e92a8b03f04e2371167f48db1310c34ff8d7e2c
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2026-04-09 09:11:19 +0000

    Implemented portfolio interface

--- portfolio_manager/interfaces/portfolioInterface.py
+++ portfolio_manager/interfaces/portfolioInterface.py
@@ -15,21 +15,60 @@
 from typing import Set, Iterable
 from .accountInterface import accountInterface
 from .securityInterface import securityInterface
+
 class portfolioInterface():
     def __init__(self, portfolioName: str, accounts: Set[accountInterface]) -> None:
-        pass
+        self.portfolioName = portfolioName
+        self.accounts = { accountItem.getName(): accountItem for accountItem in accounts }
+
+    def getPortfolioName(self) -> str:
+        return self.portfolioName
 
     def getAllAccounts(self) -> Iterable[accountInterface]:
-        pass
+        return list(self.accounts.values())
 
     #Return a collection of accounts that contain have a security or account name in the filter set.
     #If the security filter set is empty only the account filters will be used.
     #If the account filter set is empty on the security filters will be used.
     def getAccounts(self, accountNamesFilter:Set[str], securitiesFilter:Set) -> Iterable[accountInterface]:
-        pass 
+        matchingNameFilter = []
+        matchingSecurityFilter = []
+
+        if not accountNamesFilter and not securitiesFilter:
+            return list(self.accounts.values())
+
+        for accountName in accountNamesFilter:
+            if accountName in self.accounts:
+                matchingNameFilter.append(self.accounts[accountName])
+
+        for accountName in self.accounts:
+            if self.accounts[accountName].getPositions(securitiesFilter) != {}:
+                matchingSecurityFilter.append(self.accounts[accountName])
+
+        if not accountNamesFilter:
+            return matchingSecurityFilter
+
+        if not securitiesFilter:
+            return matchingNameFilter
+
+        res = []
+
+        print(accountNamesFilter)
+        print(securitiesFilter)
+        print(matchingNameFilter)
+        print(matchingSecurityFilter)
+
+        for account in matchingNameFilter:
+            if account in matchingSecurityFilter:
+                res.append(account)
+
+        return res
 
     def addAccounts(self, accounts: Set[accountInterface]) -> None:
-        pass
+        for account in accounts:
+            self.accounts[account.getName()] = account
+
 
     def removeAccounts(self, accountNames: Set[str]) -> None:
-        pass
+        for accountName in accountNames:
+            self.accounts.pop(accountName, "")
